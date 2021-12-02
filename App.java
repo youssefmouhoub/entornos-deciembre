25f8de02f2850032891d3bfe81ed6f194c62ed42
youssef <youssefleon75rgmail.com>
2021-12-02 13:37:23 +0100

    color favorito agregado

--- App.java
+++ App.java
@@ -2,8 +2,9 @@ public class App {
   
   public static void main(String[] args) {
     System.out.println("Youssef");
-    System.out.println("Contenido 3");
+    System.out.println("León");
     System.out.println("02-12-2021");
+    System.out.println("Color favorito: Negro");
   }
   
 }
